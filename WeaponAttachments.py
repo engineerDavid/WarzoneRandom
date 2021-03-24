bd6f2918b28ef9d88128ee803ce9b2ac31647ce3
engineerDavid <33636080+engineerDavid@users.noreply.github.com>
2021-03-23 20:00:16 -0500

    improved documntation on weaon type

---
+++ WeaponAttachments.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+
+#This scrapper is the data from the Weapon Scrapper that gets all the weapon links from the home page
+Weaponlinks = ['https://gamewith.net/cod-coldwar/article/show/21938', 'https://gamewith.net/cod-coldwar/article/show/22111', 'https://gamewith.net/cod-coldwar/article/show/21939', 
+'https://gamewith.net/cod-coldwar/article/show/22397', 'https://gamewith.net/cod-coldwar/article/show/23226', 'https://gamewith.net/cod-coldwar/article/show/23090', 
+'https://gamewith.net/cod-coldwar/article/show/21805', 'https://gamewith.net/cod-coldwar/article/show/22042', 'https://gamewith.net/cod-coldwar/article/show/22112', 
+'https://gamewith.net/cod-coldwar/article/show/22113', 'https://gamewith.net/cod-coldwar/article/show/22398', 'https://gamewith.net/cod-coldwar/article/show/23225',
+ 'https://gamewith.net/cod-coldwar/article/show/23490', 'https://gamewith.net/cod-coldwar/article/show/21805', 'https://gamewith.net/cod-coldwar/article/show/22116', 
+ 'https://gamewith.net/cod-coldwar/article/show/22115', 'https://gamewith.net/cod-coldwar/article/show/23224', 'https://gamewith.net/cod-coldwar/article/show/23223', 
+ 'https://gamewith.net/cod-coldwar/article/show/22121', 'https://gamewith.net/cod-coldwar/article/show/22117', 'https://gamewith.net/cod-coldwar/article/show/23222', 
+ 'https://gamewith.net/cod-coldwar/article/show/22122', 'https://gamewith.net/cod-coldwar/article/show/22120', 'https://gamewith.net/cod-coldwar/article/show/23221', 
+ 'https://gamewith.net/cod-coldwar/article/show/22123', 'https://gamewith.net/cod-coldwar/article/show/22124', 'https://gamewith.net/cod-coldwar/article/show/22399', 
+ 'https://gamewith.net/cod-coldwar/article/show/22125', 'https://gamewith.net/cod-coldwar/article/show/22127', 'https://gamewith.net/cod-coldwar/article/show/23798', 
+ 'https://gamewith.net/cod-coldwar/article/show/22129', 'https://gamewith.net/cod-coldwar/article/show/22400', 'https://gamewith.net/cod-coldwar/article/show/23894', 
+ 'https://gamewith.net/cod-coldwar/article/show/23896', 'https://gamewith.net/cod-coldwar/article/show/23488']
+
+for r in range(len(WeaponClasslinks)):
+    # page = requests.get(WeaponClasslinks[r])
+
+    # soup = BeautifulSoup(page.content, 'html.parser')
+    # results = soup.find('div', attrs={'class':'codcw_weapons'})
+    # table = results.find('table')
+    # table_body = table.find('tbody')
+    
+    # print(table_body)
+    # #The urls for diffrent cold war guns
+    # WeaponUrls=[]
+    # break
+    url = WeaponClasslinks[r]
+    r = requests.get(url)
+    df_list = pd.read_html(r.text) # this parses all the tables in webpages to a list
+    df = df_list[1]
+    print(df)
+    df.head()
+    break
+
+
+    #get the div id from the html file
+    # for ultag in soup.find(class='codcw_weapons'):
+    #     #find the a's within
+    #     for litag in ultag.find_all('a'):
+    #         #get the href
+    #         if litag.has_attr('href'):
+    #             CWurls.append(ColdWarurl+litag.attrs['href'])
+
+    # print(CWurls)
--- WeaponType.py
+++ WeaponType.py
@@ -1,28 +1,29 @@
-import requests 
-
+import pandas as pd
+import requests
 from bs4 import BeautifulSoup
 
-#This scrapper gets all the links for each diffrent wepon type.
+#3/23/2021
+#This scrapper gets all the links for each diffrent weapon type.
 
-WeaponClasslinks = ['https://gamewith.net/cod-coldwar/article/show/21943#NEW', 'https://gamewith.net/cod-coldwar/article/show/21943#AR', 'https://gamewith.net/cod-coldwar/article/show/21943#SMG', 
-'https://gamewith.net/cod-coldwar/article/show/21943#TR', 'https://gamewith.net/cod-coldwar/article/show/21943#LMG', 'https://gamewith.net/cod-coldwar/article/show/21943#SR', 'https://gamewith.net/cod-coldwar/article/show/21943#SR', 'https://gamewith.net/cod-coldwar/article/show/21943#HG', 
-'https://gamewith.net/cod-coldwar/article/show/21943#SG', 'https://gamewith.net/cod-coldwar/article/show/21943#LN', 'https://gamewith.net/cod-coldwar/article/show/21943#ML']
+#home link for wepons
+Weaponslink = 'https://gamewith.net/cod-coldwar/article/show/21943#New'
 
-for r in range(len(WeaponClasslinks)):
-    page = requests.get(WeaponClasslinks[r])
+page = requests.get(Weaponslink)
 
-    soup = BeautifulSoup(page.content, 'html.parser')
-    results = soup.find(class='codcw_weapons')
-    print(results)
-    #The urls for diffrent cold war guns
-    WeaponUrls=[]
+soup = BeautifulSoup(page.content, 'html.parser')
 
-    #get the div id from the html file
-    # for ultag in soup.find(class='codcw_weapons'):
-    #     #find the a's within
-    #     for litag in ultag.find_all('a'):
-    #         #get the href
-    #         if litag.has_attr('href'):
-    #             CWurls.append(ColdWarurl+litag.attrs['href'])
+#list to keep track of all the links for the 35 diffrent weapons in warzone
+WeaponUrls=[]
 
-    # print(CWurls)
+
+#get the div id from the html file
+for ultag in soup.find_all('div', attrs={'class':'codcw_weapons'}):
+    #find the a's within the class
+    for litag in ultag.find_all('a'):
+        #get the link from the <a>
+        if litag.has_attr('href'):
+            WeaponUrls.append(litag.attrs['href'])
+
+    
+
+ 
