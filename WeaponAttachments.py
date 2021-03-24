7191cf58889e14e18789ef0d6c36bd59112dec18
engineerDavid <33636080+engineerDavid@users.noreply.github.com>
2021-03-24 00:00:05 -0500

    was able to get most of the data but there are outliers

--- WeaponAttachments.py
+++ WeaponAttachments.py
@@ -6,16 +6,16 @@ import pandas as pd
 #This scrapper is the data from the Weapon Scrapper that gets all the weapon links from the home page
 Weaponlinks = ['https://gamewith.net/cod-coldwar/article/show/21938', 'https://gamewith.net/cod-coldwar/article/show/22111', 'https://gamewith.net/cod-coldwar/article/show/21939', 
 'https://gamewith.net/cod-coldwar/article/show/22397', 'https://gamewith.net/cod-coldwar/article/show/23226', 'https://gamewith.net/cod-coldwar/article/show/23090', 
-'https://gamewith.net/cod-coldwar/article/show/21805', 'https://gamewith.net/cod-coldwar/article/show/22042', 'https://gamewith.net/cod-coldwar/article/show/22112', 
+ 'https://gamewith.net/cod-coldwar/article/show/22042', 'https://gamewith.net/cod-coldwar/article/show/22112', 
 'https://gamewith.net/cod-coldwar/article/show/22113', 'https://gamewith.net/cod-coldwar/article/show/22398', 'https://gamewith.net/cod-coldwar/article/show/23225',
- 'https://gamewith.net/cod-coldwar/article/show/23490', 'https://gamewith.net/cod-coldwar/article/show/21805', 'https://gamewith.net/cod-coldwar/article/show/22116', 
+ 'https://gamewith.net/cod-coldwar/article/show/23490',  'https://gamewith.net/cod-coldwar/article/show/22116', 
  'https://gamewith.net/cod-coldwar/article/show/22115', 'https://gamewith.net/cod-coldwar/article/show/23224', 'https://gamewith.net/cod-coldwar/article/show/23223', 
  'https://gamewith.net/cod-coldwar/article/show/22121', 'https://gamewith.net/cod-coldwar/article/show/22117', 'https://gamewith.net/cod-coldwar/article/show/23222', 
  'https://gamewith.net/cod-coldwar/article/show/22122', 'https://gamewith.net/cod-coldwar/article/show/22120', 'https://gamewith.net/cod-coldwar/article/show/23221', 
  'https://gamewith.net/cod-coldwar/article/show/22123', 'https://gamewith.net/cod-coldwar/article/show/22124', 'https://gamewith.net/cod-coldwar/article/show/22399', 
  'https://gamewith.net/cod-coldwar/article/show/22125', 'https://gamewith.net/cod-coldwar/article/show/22127', 'https://gamewith.net/cod-coldwar/article/show/23798', 
  'https://gamewith.net/cod-coldwar/article/show/22129', 'https://gamewith.net/cod-coldwar/article/show/22400', 'https://gamewith.net/cod-coldwar/article/show/23894', 
- 'https://gamewith.net/cod-coldwar/article/show/23896', 'https://gamewith.net/cod-coldwar/article/show/23488']
+ 'https://gamewith.net/cod-coldwar/article/show/23896']
 
 Weapons = {}
 
@@ -44,9 +44,7 @@ def WeaponsUpdate(WeaponNumber, Name,WeaponType, WeaponFeature, UnlockLevel, Max
     }
 
     
-# WeaponsUpdate(1, "as", "as", "as", "as", "as", "as", "as", "as", "as", "as", "as", "as", "as")
-# WeaponsUpdate(2, "as", "as", "as", "as", "as", "as", "as", "as", "as", "as", "as", "as", "as")
-# print(Weapons)
+
 
 for link in range(len(Weaponlinks)):
     try:
@@ -57,20 +55,34 @@ for link in range(len(Weaponlinks)):
         page = requests.get(url)
         df_list = pd.read_html(page.text) # this parses all the tables in webpages to a list
         #Gets the weapon Name from table 2
-        Table2 = df_list[2]
-        WeaponName = Table2[1][0]
+        
+        try:
+            Table2 = df_list[2]
+            WeaponName = Table2[1][0]
+        except:
+            WeaponName = "Ksp"
+            
+            
+        print(WeaponName)
+        print(url)
+        
         #Gets the weapon type, weapon feature, unlock level, and maxlevel from table 1
         Table1 = df_list[1]
+        
         WeaponType = Table1[1][0]
         WeaponFeature = Table1[1][1]
-        UnlockLevel = Table1[1][2]
-        MaxLevel = Table1[1][3]
+        UnlockLevel = Table1[1][1]
+        try:
+            MaxLevel = Table1[1][3]
+        except:
+            WeaponName = "None"
         #pre-defind array since there are # 8 diffrent types of attatchmnets
         Attachments =[[],[],[],[],[],[],[],[]]
-        z = 7
+        #used to get the last tables of the url
+        attatchmnetsList = 7
         if len(df_list) > 7:
-            z = 9 
-        for r in range(len(df_list)-z):
+            attatchmnetsList = 9 
+        for r in range(len(df_list)-attatchmnetsList):
             tt = len(df_list)-4-r
             Table = df_list[tt]
             # print(Table)
@@ -79,16 +91,17 @@ for link in range(len(Weaponlinks)):
                     Attachments[r].append(Table['Attachment'][j])
 
             except():
-                 r = r +1 
+                 print("error")
                  
             
 
 
         WeaponsUpdate(link, WeaponName, WeaponType, WeaponFeature, UnlockLevel, MaxLevel, Attachments[0], Attachments[1], Attachments[3] , Attachments[3], 
         Attachments[4], Attachments[5], Attachments[6], Attachments[7])
+        
         with open('Sample.json', 'w') as fp:
             json.dump(Weapons, fp)
-        print(Weapons)
+        
     except():
         print("An exception occurred")
 # with open('Sample.json', 'w') as fp:
